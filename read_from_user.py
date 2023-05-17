8a4ddcb9d10a09d22910957a71020a6933497d2f
Laxmi Ganji <117654059+LaxmiGanji@users.noreply.github.com>
2023-05-17 15:16:06 +0530

    Create read_from_user.py

---
+++ read_from_user.py
@@ -0,0 +1,12 @@
+
+# Read input from the user
+name = input("Enter your name: ")
+address = input("Enter your address: ")
+email = input("Enter your email: ")
+phone_number = input("Enter your phone number: ")
+
+# Print the details
+print("Name:", name)
+print("Address:", address)
+print("Email:", email)
+print("Phone Number:", phone_number)
