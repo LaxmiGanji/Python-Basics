69e5a4d94876cb11ee082a3e3973bcab9b81b81e
Laxmi Ganji <117654059+LaxmiGanji@users.noreply.github.com>
2023-08-23 20:58:38 +0530

    Create add_letter.py

---
+++ add_letter.py
@@ -0,0 +1,7 @@
+def add_single_letter_words(wordlist):
+  wordlist.append("I")
+  wordlist.append("a")
+  return wordlist
+wordlist = ["apple", "banana", "cat"]
+wordlist = add_single_letter_words(wordlist)
+print(wordlist)
